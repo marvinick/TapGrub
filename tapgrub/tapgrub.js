333ef30ed5d4f3c04d179eadc7c37427db30efb4
marvinick <marviluw@gmail.com>
2015-09-23 20:47:37 -0700

    mongo integration

--- tapgrub/tapgrub.js
+++ tapgrub/tapgrub.js
@@ -1,14 +1,13 @@
+Taps = new Mongo.Collection('taps');
+
 if (Meteor.isClient) {
  // this code only runs on the client
  angular.module('tapgrub',['angular-meteor']);
 
- angular.module('tapgrub').controller('TapCtrl', ['$scope',
-  function ($scope) {
+ angular.module('tapgrub').controller('TapCtrl', ['$scope', '$meteor',
+  function ($scope, $meteor) {
 
-    $scope.taps = [
-      { text: 'This is task 1' },
-      { text: 'This is task 2' },
-      { text: 'This is task 3' }
-    ];
+    $scope.taps = $meteor.collection(Taps);
+
   }]);
 }
