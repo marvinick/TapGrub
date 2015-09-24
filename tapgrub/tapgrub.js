6a26856a0e8be31404edda535cb85e0cff932fdb
marvinick <marviluw@gmail.com>
2015-09-23 23:35:39 -0700

    add privacy -- error on line 47

--- tapgrub/tapgrub.js
+++ tapgrub/tapgrub.js
@@ -12,6 +12,8 @@ if (Meteor.isClient) {
  angular.module('tapgrub').controller('TapCtrl', ['$scope', '$meteor',
   function ($scope, $meteor) {
 
+    $scope.$meteorSubscribe('taps');
+
     $scope.taps = $meteor.collection(function() {
       return Taps.find($scope.getReactively('query'), { sort: { createdAt: -1 } })
     });
@@ -21,11 +23,15 @@ if (Meteor.isClient) {
     };
 
     $scope.deleteTap = function (tap) {
-      $meteor.call('deleteTap', tap_id);
+      $meteor.call('deleteTap', tap._id);
     };
 
     $scope.setChecked = function (tap) {
       $meteor.call('setChecked', tap._id, !tap.checked);
+
+    $scope.setPrivate = function (tap) {
+      $meteor.call('setPrivate', tap._id, ! tap.private);
+    };
 
     $scope.$watch('hideCompleted', function() {
       if ($scope.hideCompleted)
@@ -56,9 +62,43 @@ Meteor.methods({
     });
   },
   deleteTap: function (tapId) {
+    var tap = Taps.findOne(tapId);
+    if (tap.private && tap.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
     Taps.remove(tapId);
   },
   setChecked: function (tapId, setChecked) {
+    var tap = Taps.findOne(tapId);
+    if (tap.private && tap.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
     Taps.update(tapId, { $set: { checked: setChecked} });
+  },
+  setPrivate: function (tapId, setToPrivate) {
+    var tap = Taps.findOne(tapId);
+
+    // make sure only the tap owner can make a tap private
+    if (tap.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Taps.update(tapId, { $set: { private: setToPrivate } });
   }
-})
+});
+
+if (Meteor.isServer) {
+  Meteor.publish('taps', function () {
+    return Taps.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
+
